58dabc7fabbfc49fc1861361207a767c6de3b930
davidharnisch <davidharnisch@seznam.cz>
2023-02-13 16:10:39 +0100

    FInal edit

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function StartQuiz(props) {
   return (
       <div className='start-quiz-container'>
-          <h1 className='start-quiz-header'>Quizzical</h1>
+          <h1 className='start-quiz-header'>Quiz game</h1>
           <div className='quiz-options-container'>
           </div>
           <button className='button-base-style' onClick={props.startBtnHandler}>
